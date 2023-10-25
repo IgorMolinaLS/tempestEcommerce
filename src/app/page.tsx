3087877b8c2787eb7196af84a904e82db6294388
Igor Molina <igoramil452@gmai.com>
2023-10-25 17:00:30 -0300

    feat: add header menu using sheet component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="text-red-500 bg-blue-600">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return;
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,49 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentCircleIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <HomeIcon size={18} />
+              Inicio
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <LogInIcon size={18} />
+              Fazer login
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <PercentIcon size={18} />
+              Ofertas
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-3">
+              <ListOrderedIcon size={18} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
       <h1 className="text-lg font-semibold">
         <span className="text-primary">Tempest</span> Store
       </h1>
