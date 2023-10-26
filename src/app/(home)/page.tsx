4d286879c57ca11bf6f47847420d93334cf4b0af
Igor Molina <igoramil452@gmai.com>
2023-10-26 16:59:41 -0300

    feat: add productItem and productList components, sales carroussel added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,31 @@
 import Categories from "@/components/ui/categories";
+import ProductList from "@/components/ui/product-list";
+import { prismaClient } from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercent: {
+        gt: 0,
+      },
+    },
+  });
   return (
-    <div className="p-5">
+    <div>
       <Image
         alt="Até 55% de desconto! Só este mês"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
         src="/banner-55off.png"
       />
-      <div className="mt-8">
+      <div className="mt-8 px-5 ">
         <Categories />
+      </div>
+      <div className="mt-8 ">
+        <ProductList products={deals} />
       </div>
     </div>
   );
