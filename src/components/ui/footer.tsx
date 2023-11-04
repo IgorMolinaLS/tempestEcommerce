769a3e2ccfaeebda2bd6c83c92af927df7bd14ee
Igor Molina <igoramil452@gmai.com>
2023-11-04 11:02:55 -0300

    added catalogue page with categoryItem component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fontsize bg-accent px-8 text-[0.625rem] opacity-75">
+    <div className="fontsize bg-accent px-8 text-sm opacity-75">
       © 2023 Copyright <span className="font-semibold">Tempest Store</span>
     </div>
   );
